be112b82693f9cb4b66fe2b990597ea7135e492a
Roland Gulya <mgulyaroland@gmail.com>
2022-09-26 15:13:31 +0200

    Ioc contener for the endpoint

--- charp/MainApp/MainApp.Endpoint/Program.cs
+++ charp/MainApp/MainApp.Endpoint/Program.cs
@@ -13,8 +13,14 @@ builder.Services.AddControllers();
 builder.Services.AddTransient<StrongholdDbContext>();
 
 builder.Services.AddTransient<IRepository<WareHouse>, WarehouseRepository>();
+builder.Services.AddTransient<IRepository<Blacksmith>, BlacksmithRepository>();
+builder.Services.AddTransient<IRepository<Generalstore>, GeneralstoreRepository>();
+builder.Services.AddTransient<IRepository<Recepies>, RecepieRepository>();
 
 builder.Services.AddTransient<IWarehouseLogic, WarehouseLogic>();
+builder.Services.AddTransient<IBlacksmithLogic, BlacksmithLogic>();
+builder.Services.AddTransient<IGeneralstoreLogic, GeneralstoreLogic>();
+builder.Services.AddTransient<IRecepieLogic, RecepieLogic>();
 
 
 
