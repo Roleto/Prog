b6ee57e484e1b64ba35e6497940b541280c10cba
Roland Gulya <mgulyaroland@gmail.com>
2022-11-29 23:31:55 +0100

    Editor windows implemented

---
+++ charp/MainApp/MainApp_HFT_2021222.WPFClient/Windows/GeneralstoreEditorWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using MainApp.Models.DBModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MainApp_HFT_2021222.WPFClient.Windows
+{
+    /// <summary>
+    /// Interaction logic for GeneralstoreEditorWindow.xaml
+    /// </summary>
+    public partial class GeneralstoreEditorWindow : Window
+    {
+        public GeneralstoreEditorWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new Generalstore();
+        }
+        public GeneralstoreEditorWindow(Generalstore generalstore)
+        {
+            InitializeComponent();
+            this.DataContext = generalstore;
+
+        }
+
+        private void OkCick(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+        private void FalseCick(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+    }
+}
---
+++ charp/MainApp/MainApp_HFT_2021222.WPFClient/Windows/WharehouseEditorWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using MainApp.Models.DBModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MainApp_HFT_2021222.WPFClient.Windows
+{
+    /// <summary>
+    /// Interaction logic for WharehouseEditorWindow.xaml
+    /// </summary>
+    public partial class WharehouseEditorWindow : Window
+    {
+        public WharehouseEditorWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new WareHouse();
+        }
+        public WharehouseEditorWindow(WareHouse wareHouse)
+        {
+            InitializeComponent();
+            this.DataContext = wareHouse;
+
+        }
+
+        private void OkCick(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+        private void FalseCick(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+    }
+}
